34ef49cad2c93bf60d8a7a75f938ea6da8a4b5fd
Scyphers <scyphers.dev@gmail.com>
2023-03-28 15:32:03 +0800

    rename dots to stars for naming consistency

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 
 player_x = 2
 player_y = 4
-dots = [0, 0]
-dot_y = 4
+stars = [0, 0]
+star_y = 4
 
 presses_a = 0
 presses_b = 0
@@ -22,21 +22,21 @@ while True:
         player_x += 1
         presses_b = button_b.get_presses()
         
-    dot_y += 1
-    if dot_y > 4:
-        if player_x in dots:
+    star_y += 1
+    if star_y > 4:
+        if player_x in stars:
             break
         
-        dots[0], dots[1] = (random.randint(0, 4), random.randint(0, 4))
-        dot_y = 0
+        stars[0], stars[1] = (random.randint(0, 4), random.randint(0, 4))
+        star_y = 0
         score += 1
         if speed > 100:
             speed -= 5
     
     display.clear()
     display.set_pixel(player_x, 4, 5)
-    display.set_pixel(dots[0], dot_y, 9)
-    display.set_pixel(dots[1], dot_y, 9)
+    display.set_pixel(stars[0], star_y, 9)
+    display.set_pixel(stars[1], star_y, 9)
     
     sleep(speed)
     
