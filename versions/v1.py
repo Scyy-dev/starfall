424fd844c7679e2ad9cb087a7da17f1cdb1e1c81
Scyphers <scyphers.dev@gmail.com>
2023-06-20 19:28:50 +1000

    Add versioning for an iterative development approach

---
+++ versions/v1.py
@@ -0,0 +1,22 @@
+from microbit import *
+
+player_x = 2
+player_y = 4
+
+presses_a = 0
+presses_b = 0
+
+display.clear()
+
+while True:
+
+    if presses_a is not button_a.get_presses() and player_x > 0:
+        player_x -= 1
+        presses_a = button_a.get_presses()
+
+    if presses_b is not button_b.get_presses() and player_x < 4:
+        player_x += 1
+        presses_b = button_b.get_presses()
+
+    display.clear()
+    display.set_pixel(player_x, 4, 5)
---
+++ versions/v2.py
@@ -0,0 +1,37 @@
+from microbit import *
+import random
+
+player_x = 2
+player_y = 4
+
+stars_x = [0, 0]
+star_y = 4
+
+presses_a = 0
+presses_b = 0
+
+speed = 300
+
+display.clear()
+
+while True:
+
+    if presses_a is not button_a.get_presses() and player_x > 0:
+        player_x -= 1
+        presses_a = button_a.get_presses()
+
+    if presses_b is not button_b.get_presses() and player_x < 4:
+        player_x += 1
+        presses_b = button_b.get_presses()
+
+    star_y += 1
+    if star_y > 4:
+        stars_x[0], stars_x[1] = (random.randint(0, 4), random.randint(0, 4))
+        star_y = 0
+
+    display.clear()
+    display.set_pixel(player_x, 4, 5)
+    display.set_pixel(stars_x[0], star_y, 9)
+    display.set_pixel(stars_x[1], star_y, 9)    
+
+    sleep(speed)
